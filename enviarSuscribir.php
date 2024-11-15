0e3031ba0c2bed984c6c435fc6b35361d5b15207
Sionchez Goncalves Carlos Elias <sionchezcarlos@gmail.com>
2024-11-15 09:35:50 -0500

    Cargar Archivos

---
+++ enviarSuscribir.php
@@ -0,0 +1,35 @@
+<?php
+
+    $to = "administracion@smartcode.pe";
+    // $to = "sionchezcarlos@gmail.com";
+    
+    $from = $_REQUEST['email'];
+
+    $subject = 'Correo enviado desde www.smartcode.pe - Suscribir Usuario';
+
+    $headers = "From: $from";
+	$headers = "From: " . $from . "\r\n";
+	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+    $logo = 'img/elements/logoUno.png';
+    $link = 'https://www.smartcode.pe/';
+
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body .= "<table style='width: 100%;'>";
+	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "</td></tr></thead><tbody><tr>";
+	$body .= "<td style='border:none;'><strong>Name:</strong> ---</td>";
+	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "</tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subject}</td></tr>";
+	$body .= "<tr><td></td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'> ---</td></tr>";
+	$body .= "</tbody></table>";
+	$body .= "</body></html>";
+
+    $send = mail($to, $subject, $body, $headers);
+
+?>
